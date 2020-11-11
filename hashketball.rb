6d68b866c786b34d2ac4d8739d49db32c0ea5343
Grant Hesketh <ghesketh76@gmail.com>
2020-11-11 19:57:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,17 @@
 def game_hash
   game_hash = {
     home: {
-      team_name: "",
-      colors: [],
-      players: {}
+      team_name: "Brooklyn Nets",
+      colors: ["Black","White"],
+      players: {
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          
+        }
+      }
     },
     away: {
       team_name: "",
